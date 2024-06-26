6e18a51626a0fc53f907d47d51139ce1a32a22d3
Ar1234B <shifanding1@qq.com>
2024-06-27 00:32:09 +0800

    Create unfw.cpp
    Nine four car.

---
+++ unfw.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int age = 18;
+    if(age >= 18){
+        cout << "可以进入" << endl;
+    }else{
+        cout << "禁止进入!!!" << endl;
+    }
+    return 0;
+}
